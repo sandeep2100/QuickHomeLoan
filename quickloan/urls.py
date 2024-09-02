468595477b11bf44b0de3cd796305fc7ebcaace3
sandeep2100 <sandeep@godbms.com>
2024-09-02 22:33:51 +0530

    initial

--- quickloan/urls.py
+++ quickloan/urls.py
@@ -4,5 +4,9 @@ from .views import *
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
-    path("contact-us/", contactus),
+    path("contact-us/", contactus, name="contact-us"),
+    path("loan_program/", loan_program, name="loan_program"),
+    path("loan_program_detail/", loan_program_detail, name="loan_program_detail"),
+    path("calculator/", calculator, name="calculator"),
+
 ]
--- quickloan/views.py
+++ quickloan/views.py
@@ -11,3 +11,15 @@ def about(request):
 
 def contactus(request):
     return render(request, "contact.html")
+
+
+def loan_program(request):
+    return render(request, "loan_program.html")
+
+
+def loan_program_detail(request):
+    return render(request, "loan_program_detail.html")
+
+
+def calculator(request):
+    return render(request, "calculator.html")
